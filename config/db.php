cbc4aad5ea1d9c31b36c22fb55b2bf9046605626
Kosalrak <linnorak.k@aditi.asia>
2025-05-03 12:26:43 +0700

    new update the code script 134

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+$db_host = '127.0.0.1';
+$db_port = 3306; 
+$db_user = 'root';
+$db_password = 'Rak077871078';
+$db_db = 'task_management';
+
+try {
+    $pdo = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_db", $db_user, $db_password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec("SET NAMES 'utf8'");
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>

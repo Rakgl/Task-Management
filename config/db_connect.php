8e3410e2e36fbaae8ddc6310bb3da4a12c07f8c9
Kosalrak <linnorak.k@aditi.asia>
2025-05-05 17:40:44 +0700

    new update the code script 134

--- config/db_connect.php
+++ config/db_connect.php
@@ -1,13 +1,9 @@
 <?php
-<<<<<<< HEAD
 $db_host = 'norak-mysql-rty4hc';
-=======
-$db_host = '127.0.0.1';
->>>>>>> 083977bb5b0ea17771770ddac391cfdbf011cde4
 $db_port = 3306; 
 $db_user = 'root';
-$db_password = 'Rak077871078';
-$db_db = 'task_management';
+$db_password = 'root';
+$db_db = 'norak';
 
 try {
     $pdo = new PDO("mysql:host=$db_host;port=$db_port;dbname=$db_db", $db_user, $db_password);
